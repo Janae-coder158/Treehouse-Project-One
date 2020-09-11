44faa2a4829d98eca87ac5a8d98ce8015c3ce0ca
Janae-coder158 <64805655+Janae-coder158@users.noreply.github.com>
2020-09-11 09:06:02 -0400

    Update script.js

--- a_random_quote_generator-v1 (1)/js/script.js
+++ a_random_quote_generator-v1 (1)/js/script.js
@@ -3,41 +3,69 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+//reult will be: series of quotes displaying when button pushed
 
 /*** 
- * `quotes` array 
+ 
 ***/
-let quotes = [];  {
-  //quoteOne and quoteTwo from https://www.goodhousekeeping.com/life/g28470640/best-feminist-quotes/?slide=7
-  quoteOne =  ['"Each time a woman stands up for herself, without knowing it possibly, without claiming it, she stands up for all women."', 'Maya Angelou'],
-  quoteTwo =  ['"You deserve to be here. You deserve to exist. You deserve to take up space in this world of men."', 'Mackenzi Lee'],
- // quoteThree-quoteFive from https://www.codeofliving.com/blog/55-powerful-short-quotes-sayings-life/
-  quoteThree = ['"Every moment is a fresh beginning."', 'T.S Eliot'],
-  quoteFour = ['"And still, I rise."', 'Maya Angelou', 'And Still I Rise', 1978],
-  quoteFive = ['"Turn your wounds into wisdom."', 'Oprah Winfrey']
-  };
-
-
+var quotes = [ 
+  {
+  //quoteOne and quoteTwo from https://www.goodhousekeeping.com/life/
+  quote:'"Each time a woman stands up for herself, without knowing it possibly, without claiming it, she stands up for all women."', 
+  source: 'Maya Angelou'
+  },
+  {
+  quote:'"You deserve to be here. You deserve to exist. You deserve to take up space in this world of men."',
+  source: 'Mackenzi Lee'
+  },
+ // quoteThree-quoteFive from https://www.codeofliving.com/blog/
+ {
+    quote:'"Every moment is a fresh beginning."', 
+    source:'T.S Eliot'
+ },
+ {
+  quote:'"And still, I rise."',
+  source: 'Maya Angelou', 
+  citation: 'And Still I Rise', 
+  year: 1978
+ },
+{
+  quote:'"Turn your wounds into wisdom."',
+  source: 'Oprah Winfrey'
+}
+];
 
 
 /***
  * `getRandomQuote` function
+ * creates function to keep picking random quotes from above for length of array
+ * formula for function from https://css-tricks.com/snippets/javascript/select-random-item-array/
 ***/
+function getRandomQuote () {
+  var randomQuote = Math.floor(Math.random() * quotes.length);
+  return quotes[randomQuote];
+  console.log(getRandomQuote);
+}
+getRandomQuote()
 
+//print quote to get random quote to be visible on screen in html format
+function printQuote () {  
+  var randomObject = getRandomQuote();
 
+  //HTML snippet from Treehouse instructions
+  var paragraph = '<p class="quote">' + randomObject.quote + '</p>'; paragraph += '<p class="source">' + randomObject.source;
 
-/***
- * `printQuote` function
-***/
+  if ("citation" in randomObject) {
+    paragraph += '<span class="citation">' + randomObject.citation + '</span>';
+  }
 
+  if ("year" in randomObject) {
+    paragraph += '<span class="year">' + randomObject.year + '</span>' + '</p>';
+  }
 
+  document.getElementById('quote-box').innerHTML = paragraph;
+}
 
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
-***/
+  printQuote();
 
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+  document.getElementById('load-quote').addEventListener("click", printQuote, false);
